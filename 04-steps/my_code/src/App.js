0dc02cfa9a26701b0a03a7cd1dc3b3001a5888c6
Romain SALVAN <romain.salvan@g-mail.fr>
2025-07-14 10:10:51 +0200

    working on old step project to showcase children prop

--- 04-steps/my_code/src/App.js
+++ 04-steps/my_code/src/App.js
@@ -9,6 +9,9 @@ const messages = [
 export default function App() {
     return <>
         <Step/>
+        {/*<Message step={1}>*/}
+        {/*    <p>Pass in content</p>*/}
+        {/*</Message>*/}
         {/*<Step/>*/}
     </>
 }
@@ -39,20 +42,49 @@ function Step() {
                         <div className={step >= 3 ? "active" : ""}>3</div>
                     </div>
 
-                    <p className="message">Step {step}: {messages[step - 1]}</p>
+                    <Message step={step}>
+                        {messages[step-1]}
+                        <div className="buttons">
+                            <Button
+                                bgColor="#e7e7e7"
+                                textColor="#333"
+                                onClick={()=>alert(`Learn how to ${messages[step - 1]}`)}
+                            >
+                                Click me
+                            </Button>
+                        </div>
+                    </Message>
 
                     <div className="buttons">
-                        <button style={{backgroundColor: '#7950f2', color: '#fff'}}
-                                onClick={handlePrevious}>
-                            Previous
-                        </button>
-                        <button style={{backgroundColor: '#7950f2', color: '#fff'}}
-                                onClick={handleNext}>
-                            Next
-                        </button>
+                        <Button textColor="#fff" bgColor='#7950f2' onClick={handlePrevious}>
+                            <span>👈</span> Previous <span>😊</span>
+                        </Button>
+                        <Button textColor="#fff" bgColor='#7950f2' onClick={handleNext}>
+                            <span>👉</span> Next <span>❤️️</span>
+                        </Button>
                     </div>
                 </div>
             )}
         </>
     )
+}
+
+function Button({textColor, bgColor, onClick, children}) {
+    return (
+        <button
+            style={{backgroundColor: bgColor, color: textColor}}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    )
+}
+
+function Message({step, children}){
+    return (
+        <div className="message">
+            <h3>Step {step}:</h3>
+            {children}
+        </div>
+        )
 }
