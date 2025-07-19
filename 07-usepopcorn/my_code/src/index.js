5b41a1b7d34a8f090732915e08950e4d5e61c247
Romain SALVAN <romain.salvan@g-mail.fr>
2025-07-19 23:28:23 +0200

    reusable start component with good apis

--- 07-usepopcorn/my_code/src/index.js
+++ 07-usepopcorn/my_code/src/index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import StarRating from "./components/StarRating";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App/>
+        {/*<App/>*/}
+        <StarRating maxRating={10}/>
+        <StarRating
+            maxRating={4}
+            color={"red"}
+            messages={["Ugly", "Mehh", "Okay", "Nice"]}
+            defaultRating={3}
+            onSetRating
+        />
     </React.StrictMode>
 );
