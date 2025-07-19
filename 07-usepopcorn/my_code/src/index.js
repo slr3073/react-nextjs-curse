85b3cbe63350d46653d55dfdcc25ab7a2cfa4026
Romain SALVAN <romain.salvan@g-mail.fr>
2025-07-19 23:33:48 +0200

    section done : props types

--- 07-usepopcorn/my_code/src/index.js
+++ 07-usepopcorn/my_code/src/index.js
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         {/*<App/>*/}
-        <StarRating maxRating={10}/>
+        <StarRating />
         <StarRating
             maxRating={4}
             color={"red"}
