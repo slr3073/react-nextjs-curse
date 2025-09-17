b0c74ee3d12725cd03a3804546a3ffa5367473d5
Romain SALVAN <romain.salvan@g-mail.fr>
2025-09-17 09:42:55 +0200

    over

--- 08-how-react-works/my_code/src/App.js
+++ 08-how-react-works/my_code/src/App.js
@@ -67,7 +67,27 @@ function TabContent({item}) {
     const [likes, setLikes] = useState(0);
 
     function handleInc() {
-        setLikes(likes + 1);
+        setLikes(likes => likes + 1)
+    }
+
+    function handleTripleInc(){
+        // setLikes(likes + 1);
+        // console.log(likes)
+        // setLikes(likes + 1);
+        // setLikes(likes + 1); // will only re-render onces  and will not work properly
+
+        setLikes(likes => likes + 1)
+        setLikes(likes => likes + 1)
+        setLikes(likes => likes + 1)
+    }
+
+    function handleUndo(){
+        setLikes(0)
+        setShowDetails(true)
+    }
+
+    function handleUndoLater(){
+        setTimeout(handleUndo, 2000)
     }
 
     return (
@@ -83,13 +103,13 @@ function TabContent({item}) {
                 <div className="hearts-counter">
                     <span>{likes} ❤️</span>
                     <button onClick={handleInc}>+</button>
-                    <button>+++</button>
+                    <button onClick={handleTripleInc}>+++</button>
                 </div>
             </div>
 
             <div className="tab-undo">
-                <button>Undo</button>
-                <button>Undo in 2s</button>
+                <button onClick={handleUndo}>Undo</button>
+                <button onClick={handleUndoLater}>Undo in 2s</button>
             </div>
         </div>
     );
