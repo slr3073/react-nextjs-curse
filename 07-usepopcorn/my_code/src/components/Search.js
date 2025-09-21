4851d4a0df096cd22547acef5819fbe2d08042e8
Romain SALVAN <romain.salvan@g-mail.fr>
2025-09-21 17:08:07 +0200

    focus on search bar the wrong way (direct dom manip)

--- 07-usepopcorn/my_code/src/components/Search.js
+++ 07-usepopcorn/my_code/src/components/Search.js
@@ -1,4 +1,13 @@
+import {useEffect} from "react";
+
 export function Search({query, setQuery}) {
+
+    useEffect(() => {
+        const el = document.querySelector(".search")
+        console.log(el)
+        el.focus()
+    }, []);
+
     return (
         <input
             className="search"
