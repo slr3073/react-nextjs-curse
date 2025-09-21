56218086993ada734f32fbfbd1bb28f598664d9e
Romain SALVAN <romain.salvan@g-mail.fr>
2025-09-21 23:04:41 +0200

    useGeolocate custom hook

--- challenges/06-use-geolocate/src/App.js
+++ challenges/06-use-geolocate/src/App.js
@@ -1,41 +1,18 @@
 import {useState} from "react";
-
-function useGeolocation() {
-}
+import {useGeolocation} from "./hooks/useGeolocate";
 
 export default function App() {
-    const [isLoading, setIsLoading] = useState(false);
     const [countClicks, setCountClicks] = useState(0);
-    const [position, setPosition] = useState({});
-    const [error, setError] = useState(null);
+    const {position: {lat, lng}, error, isLoading, getPosition} = useGeolocation()
 
-    const {lat, lng} = position;
-
-    function getPosition() {
+    function handleClick() {
         setCountClicks((count) => count + 1);
-
-        if (!navigator.geolocation)
-            return setError("Your browser does not support geolocation");
-
-        setIsLoading(true);
-        navigator.geolocation.getCurrentPosition(
-            (pos) => {
-                setPosition({
-                    lat: pos.coords.latitude,
-                    lng: pos.coords.longitude
-                });
-                setIsLoading(false);
-            },
-            (error) => {
-                setError(error.message);
-                setIsLoading(false);
-            }
-        );
+        getPosition()
     }
 
     return (
         <div>
-            <button onClick={getPosition} disabled={isLoading}>
+            <button onClick={handleClick} disabled={isLoading}>
                 Get my position
             </button>
 
---
+++ challenges/06-use-geolocate/src/hooks/useGeolocate.js
@@ -0,0 +1,30 @@
+import {useState} from "react";
+
+export function useGeolocation() {
+    const [position, setPosition] = useState({});
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    function getPosition() {
+        if (!navigator.geolocation)
+            return setError("Your browser does not support geolocation");
+
+        setIsLoading(true);
+        navigator.geolocation.getCurrentPosition(
+            (pos) => {
+                setPosition({
+                    lat: pos.coords.latitude,
+                    lng: pos.coords.longitude
+                });
+                setIsLoading(false);
+            },
+            (error) => {
+                setError(error.message);
+                setIsLoading(false);
+            }
+        );
+    }
+
+
+    return {position, error, isLoading, getPosition}
+}
